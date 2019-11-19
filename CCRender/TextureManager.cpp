e02593538fc337f843f22fffb59e208300e453bc
zchajax <zchajax@gmail.com>
2019-11-19 13:55:32 +0800

    Create TextureManager.cpp

---
+++ CCRender/TextureManager.cpp
@@ -0,0 +1,54 @@
+#include "TextureManager.h"
+#include "GameApp.h"
+
+TextureManager* TextureManager::m_pInstance = NULL;
+
+TextureManager::TextureManager()
+{
+	
+}
+
+TextureManager::~TextureManager()
+{
+	for (auto texture : m_mTextures)
+	{
+		texture.second->Release();
+	}
+	m_mTextures.clear();
+}
+
+HRESULT TextureManager::LoadTexture(const char* path)
+{
+	HRESULT hr = S_OK;
+	
+	std::map<std::string, ID3D11ShaderResourceView*>::iterator iter;
+	if ((iter = m_mTextures.find(path)) != m_mTextures.end())
+	{
+		return S_OK;
+	}
+
+	GameApp* app = GameApp::getInstance();
+	
+	ID3DShaderResourceView* tempTexture = NULL;
+	hr = app->CreateShaderResourceViewFromFile(app->AnsiToUnicode(path), NULL, NULL, &tempTexture, NULL);
+	if (FAILED(hr))
+	{
+		return S_FALSE;
+	}	
+
+	m_mTextures[path] = tempTexture;
+
+	return S_OK;
+}
+
+ID3D11ShaderResourceView* TextureManager::GetShaderResourceView(const char* path)
+{
+	ID3D11ShaderResourceView* temp = NULL;
+
+	if (m_mTextures.find(path) != m_mTextures.end())
+	{
+		temp = 	m_mTextures[path];
+	}
+
+	return path;
+}
