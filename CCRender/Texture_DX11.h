c1dee12d13117527e941bc165b95ce68ead93d58
zchajax <zchajax@gmail.com>
2019-11-19 10:06:44 +0800

    Create Texture_DX11.h

---
+++ CCRender/Texture_DX11.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <d3d11_1.h>
+
+class TEXTURE_DX11
+{
+public:
+
+	TEXTURE_DX11();
+	
+	TEXTURE_DX11(
+		UINT32		uiWidth,
+		UINT32		uiHeight,
+		DXGI_FORMAT	eFormat,
+		UINT32		nMipMapLevels,
+		BOOL		bRenderTarget	
+	);
+
+	~TEXTURE_DX11();
+
+	void			SetFilterType(D3D11_FILTER eFilterType) { _Filter = eFilterType; }
+	void			SetAddressUType(D3D11_TEXTURE_ADDRESS_MODE eAddressType) { _AddressU = eAddressType; }
+	void			SetAddressVType(D3D11_TEXTURE_ADDRESS_MODE eAddressType) { _AddressV = eAddressType; }
+
+	ID3D11Texture2D*	GetD3DTexture() { return _pD3DTexture; }
+	ID3D11SamplerState*	GetSamplerState() { return _pD3DSampler; }
+	ID3D11SamplerState*	GetShaderResource() { return _pShaderResourceView; }
+
+	UNIT32			GetWidth() { return _uiWidth; }
+	UNIT32			GetHeight() { return _uiWidth; }
+
+private:
+
+	void			Create();
+	void			Release();
+	void			CreateSampler();
+
+private:
+
+	UINT32			_uiWidth;
+	UINT32			_uiHeight;
+	DXGI_FORMAT		_Format;
+	UINT32			_uiMipMapLevels;
+
+	D3D11_FILTER		_Filter;
+	D3D11_TEXTURE_ADDRESS_MODE _AddressU;
+	D3D11_TEXTURE_ADDRESS_MODE _AddressV;
+	FLOAT			_MipLoadBias;
+	D3D11_COMPARISON_FUNC	_ComparisionFunc;
+	
+	BOOL			_bRenderTarget;
+
+	friend class		RENDER_TARGET;
+	friend class		SWAP_CHAIN;
+	friend class		CascadedShadowMap;
+};
