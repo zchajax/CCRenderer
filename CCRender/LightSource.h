6a618721fd0d86162a9047feeaa188543183ec6b
zchajax <zchajax@gmail.com>
2019-11-20 17:40:42 +0800

    Create LightSource.h

---
+++ CCRender/LightSource.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <d3d11_1.h>
+#include <xnamath.h>
+
+class LightSource
+{
+public:
+	
+	LightSource();
+	
+	~LightSource();
+
+	void Init();
+
+	void Update(float delta);
+
+	void Render();
+
+	void SetViewParams(const XMFLOAT4& Position, const XMFLOAT& LookAt, const XMFLOAT4& Up);
+
+	void SetProjParams(FLOAT Width,
+		FLOAT Height,
+		FLOAT NearZ,
+		FLOAT FarZ);
+
+	void SetLightDir(const XMFLOAT4& dir)
+	{
+		m_LightDir = dir;
+	}
+
+	const XMFLOAT4& GetPosition() const
+	{
+		return m_Position;
+	}
+
+	const XMFLOAT4& GetLookAt() const
+	{
+		return m_LookAt;
+	}
+
+	const XMFLOAT4X4& GetLightProj() const
+	{
+		return m_LightProj;
+	}
+
+	void SetDiffuseColor(XMFLOAT4& diffuseColor)
+	{
+		m_LightDir = diffuseColor;
+	}
+
+	const XMFLOAT4& GetDiffuseColor() const
+	{
+		return m_LightColor;
+	}
+
+private:
+
+	struct ConstantLightBuffer
+	{
+		XMFLOAT4 vAmbientColor;
+		XMFLOAT4 vLightDir;
+		XMFLOAT4 vLightColor;
+		XMMATRIX mLightView;
+		XMMATRIX mLightProj;
+		FLOAT Shiniess;
+		FLOAT padding1;
+		FLOAT padding2;
+		FLOAT padding3;
+	}
+
+	void CaculateViewMatrix();
+
+	void CaculateProjMatrix();
+
+	ID3D11Buffer*	m_pConstantBuffer;
+
+	XMFLOAT4	m_LightDir;
+	XMFLOAT4	m_Position;
+	XMFLOAT4	m_LookAt;
+	XMFLOAT4	m_Up;
+	FLOAT		m_Width;
+	FLOAT		m_Height;
+	FLOAT		m_NearZ;
+	FLOAT		m_FarZ;
+	XMFLOAT4X4	m_LightView;
+	XMFLOAT4X4	m_LightProj;
+
+	XMFLOAT4	m_AmbientColor;
+	XMFLOAT4	m_LightColor;
+	FLOAT		m_Shininess;
+};
