f3c91745c292301160f99af37db8158a7b640a43
zchajax <zchajax@gmail.com>
2019-11-21 17:54:34 +0800

    Create SwapChain.h

---
+++ CCRender/SwapChain.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <d3d11_1.h>
+
+class RENDER_TARGET;
+class SWAP_CHAIN
+{
+public:
+
+	SWAP_CHAIN(HWND hWnd);
+
+	~SWAP_CHAIN();
+
+	void Present();
+
+	RENDER_TARGET* GetFrontBuffer() { return m_pFrontBuffer; }
+
+	RENDER_TARGET* GetDepthBuffer() { return m_pDepthBuffer; }
+
+private:
+
+	void Create();
+
+	void Release();
+
+private:
+
+	IDXGISwapChain*	m_pSwapChain;
+	RENDER_TARGET*	m_pFrontBuffer;
+	RENDER_TARGET*	m_pDepthBuffer;
+	HWND		m_hWnd;
+};
